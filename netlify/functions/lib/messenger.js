6f391232061bdf5e903b4cdf68c4daa12aae4b7f
khoinguyen111 <kan140@scarletmail.rutgers.edu>
2025-09-05 15:05:47 -0400

    add page Id in messenger.js

--- netlify/functions/lib/messenger.js
+++ netlify/functions/lib/messenger.js
@@ -1,5 +1,7 @@
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const GRAPH_BASE = "https://graph.facebook.com/v18.0";
+const PAGE_ID = process.env.PAGE_ID;
+
 
 async function callGraph(body) {
     const r = await fetch(`${GRAPH_BASE}/me/messages?access_token=${PAGE_ACCESS_TOKEN}`, {
