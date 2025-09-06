2096d5d558f00731320e14ae4286f92f6e947830
khoinguyen111 <kan140@scarletmail.rutgers.edu>
2025-09-05 23:41:28 -0400

    modified the label

--- netlify/functions/lib/messenger.js
+++ netlify/functions/lib/messenger.js
@@ -1,7 +1,6 @@
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const GRAPH_BASE = "https://graph.facebook.com/v18.0";
 const PAGE_ID = process.env.PAGE_ID;
-const { addLabelToUser, getOrCreateLabelId, clearNeedAgentLabel } = require("./messenger");
 
 async function callGraph(body) {
     const r = await fetch(`${GRAPH_BASE}/me/messages?access_token=${PAGE_ACCESS_TOKEN}`, {
@@ -41,11 +40,6 @@ async function sendTyping(psid, on = true) {
 }
 
 async function sendQuickPriceOptions(psid) {
-
-    try {
-        const labelId = await getOrCreateLabelId("Khách Nhiều Chuyện");
-        if (labelId) await addLabelToUser(psid, labelId);
-    } catch (e) { console.log("sendQuickPriceOptions Label error:", e); }
     return callGraph({
         recipient: { id: psid },
         messaging_type: "RESPONSE",
