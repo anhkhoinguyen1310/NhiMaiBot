3dfba9854ea26f526df92f81137a8a09262662f6
khoinguyen111 <kan140@scarletmail.rutgers.edu>
2025-09-12 11:17:56 -0400

    update the apology text

--- netlify/functions/lib/messenger.js
+++ netlify/functions/lib/messenger.js
@@ -1,6 +1,7 @@
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const GRAPH_BASE = "https://graph.facebook.com/v18.0";
 const PAGE_ID = process.env.PAGE_ID;
+const { formatPrice, apologyText } = require("./lib/format");
 
 
 async function callGraph(body) {
